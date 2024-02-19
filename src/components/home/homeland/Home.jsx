913e4d6e1d4b2d8309b186a5040fd91b2fecf986
Promise Obioma <proekene16@gmail.com>
2024-02-19 13:36:50 +0100

    bigtext font change

--- src/components/home/homeland/Home.jsx
+++ src/components/home/homeland/Home.jsx
@@ -1,7 +1,7 @@
 import './home.scss'
 import Hero from '../hero/Hero'
 import Main from '../main/main/Main'
-import Footer from '../footer/Footer'
+
 const Home = () => {
   return (
     <div className='home'>
